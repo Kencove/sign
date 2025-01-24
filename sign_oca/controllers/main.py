9074efade8dec68dd9074a39fbea6cfbbee84a88
kobros-tech <alkobroslymohamed@gmail.com>
2025-01-24 22:04:16 +0300

    [18.0][IMP] sign_oca: enable signing from portal

--- sign_oca/controllers/main.py
+++ sign_oca/controllers/main.py
@@ -1,9 +1,10 @@
 from odoo import http
 from odoo.exceptions import AccessError, MissingError
-from odoo.http import request
+from odoo.http import request, route
 
 from odoo.addons.base.models.assetsbundle import AssetsBundle
 from odoo.addons.portal.controllers.portal import CustomerPortal
+from odoo.addons.portal.controllers.portal import pager as portal_pager
 
 
 class SignController(http.Controller):
@@ -108,4 +109,63 @@ class PortalSign(CustomerPortal):
             return request.redirect("/my")
         return signer_sudo.action_sign(
             items, access_token=access_token, latitude=latitude, longitude=longitude
+        )
+
+    def get_sign_requests_domain(self, request):
+        domain = [
+            ("request_id.state", "=", "sent"),
+            ("partner_id", "child_of", [request.env.user.partner_id.id]),
+            ("signed_on", "=", False),
+        ]
+        return domain
+
+    def _prepare_sign_portal_rendering_values(self, page=1, sign_page=False, **kwargs):
+        SignRequests = request.env["sign.oca.request.signer"].sudo()
+        domain = self.get_sign_requests_domain(request)
+
+        self._items_per_page = 10
+        pager_values = portal_pager(
+            url="/my/sign_requests",
+            total=SignRequests.search_count(domain),
+            page=page,
+            step=self._items_per_page,
+            url_args={},
+        )
+        sign_requests = SignRequests.search(
+            domain,
+            order="id desc",
+            limit=self._items_per_page,
+            offset=pager_values["offset"],
+        )
+        values = self._prepare_portal_layout_values()
+        values.update(
+            {
+                "sign_requests": sign_requests.sudo() if sign_page else SignRequests,
+                "page_name": "My Sign Requests",
+                "pager": pager_values,
+                "default_url": "/my/sign_requests",
+            }
+        )
+
+        return values
+
+    def _prepare_home_portal_values(self, counters):
+        values = super()._prepare_home_portal_values(counters)
+        if "sign_oca_count" in counters:
+            domain = self.get_sign_requests_domain(request)
+            SignRequests = request.env["sign.oca.request.signer"].sudo()
+            values["sign_oca_count"] = SignRequests.search_count(domain)
+        return values
+
+    @route(
+        ["/my/sign_requests", "/my/sign_requests/page/<int:page>"],
+        type="http",
+        auth="public",
+        website=True,
+    )
+    def sign_requests(self, **kwargs):
+        values = self._prepare_sign_portal_rendering_values(sign_page=True, **kwargs)
+        return request.render(
+            "sign_oca.sign_requests",
+            values,
         )
